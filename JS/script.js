b02b329f432e936ce37c6845bc165119e708f34d
Mic-9 <116760683+Mic-9@users.noreply.github.com>
2023-11-07 19:23:58 +0100

    Event delegation fix

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,7 @@ let corpo = document.querySelector('main');
 
 let restrat = document.createElement('button');
 restrat.innerHTML = '↻';
+restrat.setAttribute('id', 'res')
 corpo.appendChild(restrat);
 
 let counter = document.createElement('p');
@@ -11,36 +12,37 @@ corpo.appendChild(counter);
 let plus = document.createElement('button');
 plus.textContent = '+';
 plus.setAttribute('class', 'segni')
+plus.setAttribute('id', 'plu')
 corpo.appendChild(plus);
 
 let minus = document.createElement('button');
 minus.textContent = '-';
 minus.setAttribute('class', 'segni')
+minus.setAttribute('id', 'min')
 corpo.appendChild(minus);
 
-
+corpo.addEventListener('click', event=>{
+    if (event.target.id ==='plu'){
+        increase();
+    } if (event.target.id === 'min'){
+        decrease();
+    } if (event.target.id === 'res'){
+        toZero();
+    }
+})
 
 function increase(){
     let num = (+counter.textContent);
     num++;
     counter.textContent = num;
 };
-plus.addEventListener( 'click', ()=>{
-    increase();
-})
 
 function decrease(){
     let num = (+counter.textContent);
     num--;
     counter.textContent = num;
 };
-minus.addEventListener( 'click', ()=>{
-    decrease();
-})
 
 function toZero(){
     counter.textContent = '0';
 }
-restrat.addEventListener( 'click', ()=>{
-    toZero();
-})
